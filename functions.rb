71dae3961b421e56c1a789d0d47fc5a754d26b89
sefatkc <sefatkc37@gmail.com>
2022-05-22 12:19:11 +0300

    function definition file created

---
+++ functions.rb
@@ -0,0 +1,9 @@
+#basic function defination
+def get_username(username)
+  puts "Username: #{username}"
+end
+
+#There are two types of function calls.
+get_username('Sefa')
+get_username "Sefa"
+# can be called without parentheses
